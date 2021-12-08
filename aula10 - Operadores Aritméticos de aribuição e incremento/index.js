4828e623bebcd09b368a4ec7e7bfe136abfdb8cf
AlexBCSouza <alex.batista.91@gmail.com>
2021-12-08 15:52:28 -0300

    Operadores_contadores

---
+++ aula10 - Operadores Aritméticos de aribuição e incremento/index.js
@@ -0,0 +1,33 @@
+/* Operadores aritimético e incrementos. 
+ Incremento = ++
+ Decremento = --
+*/
+// Código não irá rodar pois (EXECUTAR UM EXMPLO DE CADA VEZ)//
+
+//Exemplo 1 
+let num1 = 10
+let num2 = 5
+console.log( num1 - num2) // Aritméticos  ( + - * / %)
+
+//Exemplo 2
+let contador = 2; // Variável
+contador ++; // 3 Incrementa mais 1 ++
+contador ++; // 4
+contador ++; // 5
+console.log(contador);
+
+//Exemplo 3
+let contador = 6; // Variável declarada
+contador **= 2;   // Incrementa conforme os operadores exemplo aritméticos(+=, -=,  *=, /=, **=).
+console.log(contador);
+
+//Exempl 4
+const passo = 50;
+let contador = 0;
+contador ++; 
+contador ++; 
+contador ++; 
+contador ++; 
+console.log(contador += passo);
+
+
